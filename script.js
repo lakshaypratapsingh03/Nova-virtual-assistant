e8bac1559c46286b03b2dffbb0c84185408f42c4
Lakshya pratap shekhawat <lakshaypratapsinghshekhawat64@gmail.com>
2025-12-25 18:20:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+
+let btn = document.querySelector("#btn");
+let content = document.querySelector("#content");
+let voice = document.querySelector("#voice");
+
+function Speak(text) {
+    let text_Speak = new SpeechSynthesisUtterance(text);
+    text_Speak.rate = 1;
+    text_Speak.pitch = 1;
+    text_Speak.volume = 1;
+    text_Speak.lang = "hi-GB";
+    window.speechSynthesis.speak(text_Speak);
+}
+
+function wishMe() {
+    let date = new Date();
+    let hours = date.getHours();
+    if (hours >= 0 && hours < 12) {
+        Speak("Good Morning sir");
+    }
+    else if (hours >= 12 && hours < 18) {
+        Speak("Good Afternoon sir");
+    }
+    else {
+        Speak("Good Evening sir");
+    }
+}
+//   window.addEventListener("load", () => {
+//    wishMe();
+//})
+
+let speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+let recognition = new speechRecognition();
+recognition.onresult = (event) => {
+    let currentIndex = event.resultIndex;
+    let transcript = event.results[currentIndex][0].transcript;
+    content.textContent = transcript;
+    takeCommand(transcript);
+}
+
+btn.addEventListener("click", () => {
+    recognition.start();
+    btn.style.display = "none";
+    voice.style.display = "block";
+}), {once: true} ;
+
+function takeCommand(message) {
+    btn.style.display = "flex";
+    voice.style.display = "none";
+    message = message.toLowerCase();
+    if (message.includes("hello nova") || message.includes("hello")) {
+        Speak("hi sir, how can I help you ");
+    }
+    else if (message.includes("how are you") || message.includes("kaise ho")) {
+        Speak("I am fine , what's about you ");
+    }
+
+    else if (message.includes("i am good") || message.includes("main theek hun") || message.includes("i am fine") || message.includes("main achha hun") || message.includes("i am well") || message.includes("i am great")) {
+        Speak("It's good to hear that , how can I help you ");
+    }
+
+    else if (message.includes("who are you") || message.includes("tell me about yourself")) {
+        Speak("I am virtual assistant created by lakshay pratap singh shekhawat & Team");
+    }
+
+    else if (message.includes("thank you nova") || message.includes("thanks")) {
+        Speak("It's my pleasure sir");
+    }
+
+    else if (message.includes("nova open youtube")) {
+        Speak("Opening YouTube");
+        window.open("https://www.youtube.com/", "_blank");
+    }
+
+    else if (message.includes("nova open telegram")) {
+        Speak("Opening Telegram");
+        window.open("https://web.telegram.org", "_blank");
+    }
+
+    else if (message.includes("nova open google")) {
+        Speak("Opening Google");
+        window.open("https://www.google.com", "_blank");
+    }
+
+    else if (message.includes("mujhe gadi de do")) {
+        Speak("sorry sir, I am not able to give you a gadi");
+    }
+
+    else if (message.includes("time")) {
+        let time = new Date().toLocaleTimeString(undefined, { hour: "numeric", minute: "numeric" });
+        Speak(time);
+    }
+
+    else if (message.includes("date")) {
+        let date = new Date().toLocaleDateString(undefined, { day: "numeric", month: "short", year: "numeric"});
+        Speak(date);
+    }
+
+
+    else {
+        let finalText = "this is what I found on internet regarding" + message.replace("nova", "");
+        Speak(finalText);
+        window.open(`https://www.google.com/search?q=${message.replace("nova", "")}`, "_blank");
+    }
+}
